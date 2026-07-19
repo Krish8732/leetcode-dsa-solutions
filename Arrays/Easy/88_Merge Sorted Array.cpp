c587b9b00260e6dfabf39164315516156d4cb014
krish <krishbadgujar05@gmail.com>
2026-07-19 12:29:11 +0530

    Arrays/Easy: 88 Merge Sorted Array

---
+++ Arrays/Easy/88_Merge Sorted Array.cpp
@@ -0,0 +1,30 @@
+// LeetCode: https://leetcode.com/problems/merge-sorted-array/
+// Concepts: Array, Two Pointers, Sorting
+
+// Approach: Compare elements from the end of both arrays and place the larger element at the end of nums1. Repeat until all elements of nums2 are merged.
+// TC: O(m + n) | SC: O(1)
+
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+
+        while (j >= 0 && i >= 0){
+            if (nums1[i] > nums2[j]){
+             nums1[k] = nums1[i];
+             i--, k--;
+            }
+            else {
+                nums1[k] = nums2[j];
+                j--, k--;
+            }
+        }
+
+        while (j >= 0){
+            nums1[k] = nums2[j];
+            j--, k--;
+        }
+    }
+};
